b4f3dd0e0672e388bb07733d81da9f192ace4038
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-05 14:25:50 +0330

    Creating a sample user on application startup

--- src/main/java/de/miladsa/springsecurityplayground/SpringSecurityPlayGroundApplication.java
+++ src/main/java/de/miladsa/springsecurityplayground/SpringSecurityPlayGroundApplication.java
@@ -1,7 +1,11 @@
 package de.miladsa.springsecurityplayground;
 
+import de.miladsa.springsecurityplayground.models.User;
+import de.miladsa.springsecurityplayground.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityPlayGroundApplication {
@@ -10,4 +14,13 @@ public class SpringSecurityPlayGroundApplication {
         SpringApplication.run(SpringSecurityPlayGroundApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository) {
+        return (args -> {
+            var user1 = new User();
+            user1.setUsername("milad");
+            user1.setPassword("123");
+            userRepository.save(user1);
+        });
+    }
 }
