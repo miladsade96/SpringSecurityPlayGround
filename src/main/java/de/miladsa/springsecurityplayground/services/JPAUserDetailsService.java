f8cec5d1a54ec6876afc6a11775a1406b3be5a50
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-05 14:25:15 +0330

    Configuring JPAUserDetailsService to load user by its username

--- src/main/java/de/miladsa/springsecurityplayground/services/JPAUserDetailsService.java
+++ src/main/java/de/miladsa/springsecurityplayground/services/JPAUserDetailsService.java
@@ -1,12 +1,26 @@
 package de.miladsa.springsecurityplayground.services;
 
+import de.miladsa.springsecurityplayground.models.User;
+import de.miladsa.springsecurityplayground.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public class JPAUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> userOptional = userRepository.findUserByUsername(username);
+        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        return new SecurityUser(user);
     }
 }
