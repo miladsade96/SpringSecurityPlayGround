dbdb0dd1857a108200671dbba04bc823e87796e6
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-05 09:14:56 +0330

    Initial commit

---
+++ src/main/java/de/miladsa/springsecurityplayground/SpringSecurityPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package de.miladsa.springsecurityplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPlayGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPlayGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/miladsa/springsecurityplayground/SpringSecurityPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package de.miladsa.springsecurityplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPlayGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
