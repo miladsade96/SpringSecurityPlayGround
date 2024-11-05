55c6cd55c846649b6180630d422b3690d9b460be
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-11-05 10:40:23 +0330

    Using JPAUserDetailsService instead of in memory

--- src/main/java/de/miladsa/springsecurityplayground/configurations/ProjectConfig.java
+++ src/main/java/de/miladsa/springsecurityplayground/configurations/ProjectConfig.java
@@ -1,25 +1,17 @@
 package de.miladsa.springsecurityplayground.configurations;
 
+import de.miladsa.springsecurityplayground.services.JPAUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService() {
-        var uds = new InMemoryUserDetailsManager();
-        var user1 = User
-                .withUsername("milad")
-                .password("123")
-                .authorities("read")
-                .build();
-        uds.createUser(user1);
-        return uds;
+        return new JPAUserDetailsService();
     }
 
     @Bean
